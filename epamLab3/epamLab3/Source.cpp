482cf598e3d3fdde065a7cc6fe238a02c192985e
IvanBoiko <deadman2002xxx@gmail.com>
2021-10-02 19:47:39 +0300

    update

--- epamLab3/epamLab3/Source.cpp
+++ epamLab3/epamLab3/Source.cpp
@@ -1,5 +1,17 @@
 #include"head.h"
 int main() {
 	srand(time(NULL) * 1000);
+	Vector first(10);
+	first.SetRandomNumbers();
+	Vector second(first);
+	second.Show();
+	second.Sort();
+	second.Show();
+	second.MinNum();
+	second.MaxNum();
+	std::cout << "\nAverage of nums is : " << second.AverageNum() << std::endl;
+	second.Task();
+	second.Show();
+	//std::cout << first[6];
 	return 0;
 }
--- epamLab3/epamLab3/head.h
+++ epamLab3/epamLab3/head.h
@@ -12,35 +12,133 @@ public:
 			std::cout << "Size of vector can not be more than 64" << std::endl;
 			this->size = 64;
 			this->array_ = new int[size];
+			for (int i{ 0 }; i < size; i++) {
+				this->array_[i] = 0;
+			}
 		}
 		else {
-			int* temp = new int[size];
-			delete this->array_;
-			this->array_ = nullptr;
-			this->array_ = temp;
-
+			if (size > 0) {
+				array_ = new int[this->size];
+				for (int i{ 0 }; i < this->size; i++)
+					this->array_[i] = 0;
+			}
+			else {
+				std::cout << "\nSize of vector can not be less than a zero";
+				this->size = 1;
+				this->array_ = new int[size];
+				for (int i{ 0 }; i < size; i++) {
+					this->array_[i] = 0;
+				}
+			}
 		}
+	}
+	Vector(Vector& obj) {
+		this->array_ = new int[obj.size];
+		this->size = obj.size;
+		this->Copy(obj);
 	}
 	~Vector() {
-		std::cout << "Vector is deleted!\n";
+		delete[] array_;
+		array_ = nullptr;
+		std::cout << "\nVector is deleted!";
 	}
-	Vector operator[](int i) {
-		return this->array_[i];
+	int& operator[](int i) {
+		return (this->array_[i]);
 	}
-	
+	Vector& Copy(Vector& obj) {
+		if (this->size >= obj.size) {
+			for (int i{ 0 }; i < obj.size; i++) {
+				this->array_[i] = obj.array_[i];
+			}
+		}
+		else {
+			for (int i{ 0 }; i < this->size; i++) {
+				this->array_[i] = obj.array_[i];
+			}
+		}
+		return *this;
+	}
 	void Show() {
 		for (int i{ 0 }; i < this->size; i++) {
-			std::cout << this->array_[i];
+			std::cout << (this->array_[i]) << " ";
 		}
+		std::cout << std::endl;
 	}
 	void SetRandomNumbers() {
 		for (int i{ 0 }; i < this->size; i++)
-			(*this)[i] = 1 + rand() % 64;
+			this->array_[i] = -64 + rand() % 128;
 	}
-	void Push_back(const int &number) {
-
+	void Push_back(const int& number) {
+		this->size++;
+		int* temp = new int[size];
+		temp[size - 1] = number;
+		for (int i{ 0 }; i < size - 1; i++) {
+			temp[i] = this->array_[i];
+		}
+		delete[]temp;
+		temp = nullptr;
+	}
+	void Sort() {
+		for (int i{ 1 }; i < this->size; i++) {
+			for (int j = i; j > 0 && (this->array_[j] < this->array_[j - 1]); j--) {
+				this->Swap(this->array_[j], this->array_[j - 1]);
+			}
+		}
+	}
+	void MinNum() {
+		this->Sort();
+		std::cout << "\nMin element is: " << (*this)[0];
+	}
+	void MaxNum() {
+		this->Sort();
+		std::cout << "\nMax element is: " << (*this)[this->size - 1];
+	}
+	double AverageNum() {
+		double sum{ 0 };
+		for (int i{ 0 }; i < this->size; i++) {
+			sum += (*this)[i];
+		}
+		sum /= this->size;
+		return sum;
+	}
+	void Swap(int& first, int& second) {
+		int temp = first;
+		first = second;
+		second = temp;
 	}
 	int Length() {
 		return this->size;
+	}
+	int GetpMinNum() {
+		int temp;
+		this->Sort();
+		temp = (*this)[this->size / 2];
+		if (temp < 0) {
+			for (int i{ this->size / 2 }; i < this->size; i++) {
+				if ((*this)[i] >= 0) {
+					temp = (*this)[i];
+					break;
+				}
+			}
+		}
+		else {
+			for (int i{ this->size / 2 }; i > 0; i--) {
+				if ((*this)[i] <= 0) {
+					temp = (*this)[i + 1];
+					break;
+				}
+			}
+		}
+		return temp;
+	}
+	void Task() {
+		//20th
+		int temp = this->GetpMinNum();
+		for (int i{ 0 }; i < this->size; i += 2) {
+			if (i > this->size) {
+				break;
+			}
+			(*this)[i] = temp;
+		}
 	}
 };
